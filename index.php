799cc28f39da4aaf1cdbb45016faf1e1dbbea519
Edevar <edevar.vip@gmail.com>
2023-10-11 15:56:40 -0300

    Primeiro Git

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+ 
+<div class="container">
+    <div class="header">
+        <h1>Gerenciador de Tarefas</h1>
+    </div>
+    <div class="form">
+        <form action="" method="get">
+            <label for="task_name"></label>
+            <input type="text" name="task_name" placeholder="Nome da Tarefa">
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+    <div class="separator">
+    </div>
+    <div class="list-tasks">
+        <ul>
+            <li>Tarefa 1</li>
+            <li>Tarefa 2</li>
+            <li>Tarefa 3</li>
+        </ul>
+    </div>
+    <div class="footer">
+        <p>Desenvolvido por Edevar</p>
+    </div>
+</div>
+    
+</body>
+</html>
